9e227ff7e3fd48da779af35148efb75e6f1ec508
Julian <julianete4@gmail.com>
2023-07-30 16:15:08 -0300

    trabajos pasados a json

--- src/pages/Trabajos/Trabajos.jsx
+++ src/pages/Trabajos/Trabajos.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import './Trabajos.css'
 import { Link as Anchor } from 'react-router-dom'
+import data from '../../data.json'
 
 export default function Trabajos() {
   return (
     <div className='trabajos' id='container-trabajos'>
       <h2>Trabajos</h2>
       <div>
-        <section className='card redSocial'>
+        {
+          data.map((obj, i) => {
+            return <section className={'card ' + obj.class} key={i}>
+              <div className='card-text'>
+                <h2>{obj.titulo}</h2>
+                <p>{obj.descripcion}</p>
+                <div className='card-icons'>
+                  <Anchor to={obj.github} target='_blank'>
+                    <div>
+                      <i className="fa-brands fa-github fa-xl"></i>
+                      <h5>Ver Codigo</h5>
+                    </div>
+                  </Anchor>
+                  <Anchor to={obj.pagina} target='_blank'>
+                    <div>
+                      <i class="fa-brands fa-chrome fa-xl"></i>
+                      <h5>Ver Página</h5>
+                    </div>
+                  </Anchor>
+                </div>
+              </div>
+            </section>
+          })
+        }
+        {/* <section className='card redSocial'>
           <div className='card-text'>
             <h2>Red Social</h2>
             <p>
@@ -28,8 +53,8 @@ export default function Trabajos() {
               </Anchor>
             </div>
           </div>
-        </section>
-        <section className='card lance'>
+        </section> */}
+        {/* <section className='card lance'>
           <div className='card-text'>
             <h2>Lance</h2>
             <p>
@@ -182,7 +207,7 @@ export default function Trabajos() {
               </Anchor>
             </div>
           </div>
-        </section>
+        </section> */}
       </div>
     </div>
   )
